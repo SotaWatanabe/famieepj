ecfc5311e7b4d1db6c08bfe2aa151c1f1547242f
渡辺創太 <w.souta.s.life@gmail.com>
2019-04-28 00:59:31 +0900

    add

--- src/content.js
+++ src/content.js
@@ -18,7 +18,6 @@ export class Content extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleDecrypt = this.handleDecrypt.bind(this);
         this.handleChangeKey = this.handleChangeKey.bind(this);
     }
 
@@ -38,8 +37,18 @@ export class Content extends Component {
                 encoded += "%" + segments[i];
             }
 
+            var key = new Buffer(this.state.key);
+            var resp = decodeURI(encoded.slice(3)).replace(/%2C/g, ",").replace(/%2F/g, "/").replace(/%2B/g, "+")
+            var buf = new Buffer(resp);
+            var iv = buf.slice(0, 16).toString();
+            var encryptedData  = buf.toString();
+
+            var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+            var decrypted = decipher.update(encryptedData, 'base64', 'utf8');
+            decrypted += decipher.final('utf8');
+
             this.setState({
-                resp: decodeURI(encoded.slice(3)).replace(/%2C/g, ",").replace(/%2F/g, "/").replace(/%2B/g, "+")
+                resp: decrypted
             })
         }).catch(e => {
             console.log(e)
@@ -53,46 +62,25 @@ export class Content extends Component {
         })
     }
 
-    handleDecrypt(e) {
-        e.preventDefault()
-        var key = new Buffer(this.state.key);
-        var buf = new Buffer(this.state.resp);
-        var iv = buf.slice(0, 16).toString();
-        var encryptedData  = buf.toString();
-
-        var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-        var decrypted = decipher.update(encryptedData, 'base64', 'utf8');
-        decrypted += decipher.final('utf8');
-        
-        this.setState({
-            resp: decrypted
-        })
-    }
-
     render() {
         return (
             <div ref={el => (this.instance = el)} className="Body">
                 <p className="explain">番号を入力して愛の誓いを確認しよう</p>
-                <p className="explain2">以下の入力欄に0xから始まるトランザクションIDを入力してください。証明書を発行する際に入力した誓いの言葉を確認することができます。</p>
+                <p className="explain2">以下の入力欄に0xから始まるトランザクションIDを入力してください。<br />その次にパートナーシップ証明書で得たパスワードを入力します。<br />誓いの文字を確認しましょう。</p>
                 <div className="form">
-                    <form name="myform" onSubmit={(e) => this.handleSubmit(e)}> 
+                    <form className="form1" name="myform" onSubmit={(e) => this.handleSubmit(e)}> 
                         <div>
-                            <input name="mytext" type="text" className="input1" onChange={(e) => this.handleChange(e)} />
+                            <input name="mytext" placeholder="0xから始まるトランザクションID" type="text" className="input1" onChange={(e) => this.handleChange(e)} />
                         </div>
-                        <div className="input2">
-                            <input type="submit" value="送信" className="input3"/>
-                        </div>
-                    </form>
                     
-                    <form name="myform" onSubmit={(e) => this.handleDecrypt(e)}> 
                         <div>
-                            <input name="mytext" type="text" className="input1" onChange={(e) => this.handleChangeKey(e)} />
+                            <input name="mytext" placeholder="復号化パスワード" type="text" className="input1" onChange={(e) => this.handleChangeKey(e)} />
                         </div>
-                        <div className="input2">
-                            <input type="submit" value="復号" className="input3"/>
-                        </div>
+                    
+                        <button type="submit"  className="input2">送信</button>
                     </form>
-                    <div　className="output">{this.state.resp}</div>
+
+                    <div　className="output">{"誓いの言葉："+ this.state.resp}</div>
                 </div>
             </div>
         )
